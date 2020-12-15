6a057d7ade08ee1b6270d96bbae78b412f9df091
danielemaraschin <danikmaraschin@gmail.com>
2020-12-15 19:43:35 +0100

    First Commands on SQL

---
+++ Commands.js
@@ -0,0 +1,8 @@
+criando novo Banco de Dados:
+CREATE DATABASE `NOME DO BANCO DE DADOS`;
+
+Selecionar  uma tabela (CITY) dentro de um banco de Dados(CLICAR EM CIMA E O DB SELECIONADO FICARÁ EM NEGRITO):
+SELECT * FROM CITY
+
+
+// = -- (comentar linha no SQL)
