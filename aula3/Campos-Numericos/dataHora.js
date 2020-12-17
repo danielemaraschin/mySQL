ccdcad6aaea559e2cf92870322add620ccdf6c7c
danielemaraschin <danikmaraschin@gmail.com>
2020-12-17 20:29:11 +0100

    Learning about Date and Time

---
+++ aula3/Campos-Numericos/dataHora.js
@@ -0,0 +1,5 @@
+Date 1000-01-01 ate 9999-12-31
+Datetime 1000-01-01 00:00:00 até 9999-12-31 23:59:59
+TIMESTAMP - 1970-01-01 00:00:01 UTC até 2038-01-19 UTC
+TIME - 838:59:59 e 839:59:59
+YEAR - 1901-2155 (pode ser expresso em 2 ou 4 digitos)
