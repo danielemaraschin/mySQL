1abf73d0725e0556c6cb55a0f62d7328ddd672a4
danielemaraschin <danikmaraschin@gmail.com>
2020-12-17 20:41:06 +0100

    String CHAR

---
+++ aula3/Campos-Numericos/Strings.js
@@ -0,0 +1,15 @@
+Campo tipo String 
+Cadeias de caracteres (texto)
+
+CHAR - Cadeia de caracteres com valor fixo (0-255)
+    CHAR(4) - "aa" - "  aa" 
+        Como está indicado 4 caracteres
+         vai completar com espaços vazios
+          para preencher os 4 espaços.
+
+          -Ocupa mais espaço de memória pq armazena espaços (q podem ser desenecessários)
+
+
+
+VARCHAR - Cadeia de caracteres com valor variado (0-255)
+    VARCHAR(4) - "aa" - "aa"
