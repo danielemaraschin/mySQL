5ebf6800d68dac831a8aa1d4271b99b4845d66c3
danielemaraschin <danikmaraschin@gmail.com>
2020-12-18 17:42:28 +0100

    Learning about Column Spacial: Geometry, Poliygon, Point and Linestring

---
+++ aula3/Spacial.js
@@ -0,0 +1,20 @@
+Campos Espaciais
+
+    Para mapas, localizações.
+
+
+SPACIAL:
+
+    -GEOMETRY
+    -POINT
+    -LINESTRING
+    -POLYGON
+
+
+    GEOMETRY e POLYGON: Representam uma área;
+    
+    POINT
+        Grava latitude e longitude.
+
+    LINESTRING
+        Representa uma linha num mapa.
