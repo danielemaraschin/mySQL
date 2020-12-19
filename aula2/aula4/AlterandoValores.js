e03ea7b5420ee3b848ae9527812c14b12af8e7da
danielemaraschin <danikmaraschin@gmail.com>
2020-12-19 15:45:01 +0100

    Changing Values

---
+++ aula2/aula4/AlterandoValores.js
@@ -0,0 +1,7 @@
+\ALTERAR VALORES CONTIDOS NA TABELA\
+
+1)Selecionar tabela :
+ USE nomeTabela
+
+UPDATE nomeTabela SET nomeCampo = 'novoValor', nomeCampo2 = novoValor
+WHERE nomeDoCampoDaIdentificação = 'valor da identificação'; (se a identificacao for um numero, sem aspas)
