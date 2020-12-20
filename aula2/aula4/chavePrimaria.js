0844a4056954c397a115f55c432b4ff64d5263ed
danielemaraschin <danikmaraschin@gmail.com>
2020-12-20 13:11:49 +0100

    Primary Key

--- aula2/aula4/chavePrimaria.js
+++ aula2/aula4/chavePrimaria.js
@@ -1,4 +1,15 @@
+Add uma chave primaria 
+
+
+ALTER TABLE nome_tabela ADD PRIMARY KEY (campoQQuerAddPK);
+->  Logo após criar a tabela (com o CREATE);
+
+
 Add uma chave primaria em uma tabela que já existe
 
 1) USE  nome_DB;
 2)ALTER TABLE nome_tabela ADD PRIMARY KEY (campoQQuerAddPK);
+
+
+
+Primary Key é o tipo CONSTRAINT  utilizada para criar uma chave primária;
