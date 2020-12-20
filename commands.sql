a8bfb6d63baafa8cd49cb723070b8994f6275461
danielemaraschin <danikmaraschin@gmail.com>
2020-12-20 08:43:47 +0100

    mySQL

---
+++ commands.sql
@@ -0,0 +1,11 @@
+#UPDATE tabela_de_vendedores SET PERCENTUAL_COMISSAO = 0.11
+#WHERE MATRICULA = '00236';
+
+#UPDATE tabela_de_vendedores SET NOME = "José Geraldo da Fonseca Junior"
+#WHERE MATRICULA = '00233'; 
+
+#delete FROM tabela_de_vendedores 
+#where MATRICULA = '00233'; 
+
+
+SELECT * FROM tabela_de_vendedores
