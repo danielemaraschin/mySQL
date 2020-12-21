2dca3fd53c171cb9f76760247ea117e869a9cba8
danielemaraschin <danikmaraschin@gmail.com>
2020-12-21 08:52:03 +0100

    working wt CREATE tb, INSERT INTO tb, ALTER tb, PK

---
+++ insert-vend-marcio.sql
@@ -0,0 +1,15 @@
+use sucos;
+
+insert into tbvendedores (MATRICULA, NOME, DATA_ADMISSAO, PERCENTUAL_COMISSAO, DE_FERIAS)
+VALUES ('00235', 'Márcio Almeida Silva', '2014-08-15', 0.08, 0);
+
+insert into tbvendedores (MATRICULA, NOME, DATA_ADMISSAO, PERCENTUAL_COMISSAO, DE_FERIAS)
+VALUES ('00236', 'Cláudia Morais', '2013-09-17', 0.08, 1);
+
+insert into tbvendedores (MATRICULA, NOME, DATA_ADMISSAO, PERCENTUAL_COMISSAO, DE_FERIAS)
+VALUES ('00237', 'Roberta Martins', '2017-03-18', 0.11, 1);
+
+insert into tbvendedores (MATRICULA, NOME, DATA_ADMISSAO, PERCENTUAL_COMISSAO, DE_FERIAS)
+VALUES ('00238', 'Péricles Alves', '2014-08-21', 0.11, 0);
+
+select * from tbvendedores;
