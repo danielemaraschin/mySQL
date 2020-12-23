592f035b2f79e64dfadffdc010b350efb28321a2
danielemaraschin <danikmaraschin@gmail.com>
2020-12-24 00:22:09 +0100

    Learning about: alias, laber, identificator

--- introd-mySQL/Notes/aula5/consultaDadosTb.js
+++ introd-mySQL/Notes/aula5/consultaDadosTb.js
@@ -14,7 +14,7 @@ selecionar somente os 5 primeiros e mostrar so o nome e cpf:
 SELECT CPF, NOME FROM tbcliente LIMIT 5;
 
 
-3) Label : 'criar' um nome pro campos
+3) Alias / Label / identificador : 'criar' um nome/apelido pro campos
 
 SELECT CPF AS CPF_CLIENTE, NOME AS NOME_CLIENTE FROM tbcliente;
 
@@ -22,3 +22,5 @@ irá aparecer em cima dos numeros /texto
      do cpf (onde estaria escrito o nome original do campo) CPF_CLIENTE
      do nome : NOME_CLIENTE
 
+=> usamos principalmente quando os campos tem nomes complicados, conforme regra da empresa, 
+então colocamos esse nome na hora da consulta para entender melhor a tabela quandop estiver consultando
