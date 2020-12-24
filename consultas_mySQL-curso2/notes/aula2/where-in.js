4a22a513682ef1bc16a2371a1441edba53ac2b33
danielemaraschin <danikmaraschin@gmail.com>
2020-12-24 01:38:49 +0100

    working with in

--- consultas_mySQL-curso2/notes/aula2/where-in.js
+++ consultas_mySQL-curso2/notes/aula2/where-in.js
@@ -9,3 +9,7 @@ select * FROM tabela_de_produtos WHERE SABOR IN ('LARANJA',  'MANGA') ;
                     é o mesmo que :
 
 select * FROM tabela_de_produtos WHERE SABOR ='LARANJA' OR SABOR = 'MANGA' ;
+
+
+select * FROM tabela_de_clientes WHERE cidade IN ('Rio de Janeiro', 'São Paulo') 
+and idade >=20;
