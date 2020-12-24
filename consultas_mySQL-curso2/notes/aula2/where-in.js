2119c94afcebd3c0f26d415dbb584e9710f333b3
danielemaraschin <danikmaraschin@gmail.com>
2020-12-24 01:45:55 +0100

    Working with Filtering

--- consultas_mySQL-curso2/notes/aula2/where-in.js
+++ consultas_mySQL-curso2/notes/aula2/where-in.js
@@ -13,3 +13,11 @@ select * FROM tabela_de_produtos WHERE SABOR ='LARANJA' OR SABOR = 'MANGA' ;
 
 select * FROM tabela_de_clientes WHERE cidade IN ('Rio de Janeiro', 'São Paulo') 
 and idade >=20;
+
+
+PODE MISTURAR COM OUTRAS CONDIÇÕES:
+
+select * FROM tabela_de_clientes WHERE cidade IN ('Rio de Janeiro', 'São Paulo') 
+and (idade >= 19 and idade <= 21);
+
+=> BOA PRÁTICA COLOCAR ENTRE PARENTESES CONDICOES QUE SÃO A RESPEITO DO MESMO CAMPO PARA FACILITAR LEITURA;
--- consultas_mySQL-curso2/sql2/filtering_not-and-or.sql
+++ consultas_mySQL-curso2/sql2/filtering_not-and-or.sql
@@ -10,5 +10,11 @@ select * FROM tabela_de_produtos WHERE SABOR = 'MANGA'
 and not ( TAMANHO = '470 ml');
 
 select * FROM tabela_de_produtos WHERE SABOR IN ('LARANJA',  'MANGA') ;
- = 
- select * FROM tabela_de_produtos WHERE SABOR ='LARANJA' OR SABOR = 'MANGA') ;
+ #= 
+ select * FROM tabela_de_produtos WHERE SABOR ='LARANJA' OR SABOR = 'MANGA' ;
+ 
+ select * FROM tabela_de_clientes WHERE cidade IN ('Rio de Janeiro', 'São Paulo') 
+and idade >=20;
+
+ select * FROM tabela_de_clientes WHERE cidade IN ('Rio de Janeiro', 'São Paulo') 
+and (idade >= 19 and idade <= 21);
