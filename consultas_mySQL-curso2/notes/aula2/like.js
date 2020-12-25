17dddfb00ba8d3c9d57970651ae76a39237022ea
danielemaraschin <danikmaraschin@gmail.com>
2020-12-25 17:53:51 +0100

    Learning about %LIKE%

--- consultas_mySQL-curso2/notes/aula2/like.js
+++ consultas_mySQL-curso2/notes/aula2/like.js
@@ -6,4 +6,6 @@ Vai selecionar todos os sabore maçãs e com maça (que contenha maça, pode ter
 SELECT * FROM tabela_de_produtos WHERE SABOR LIKE '%Maça%' and 
 embalagem = pet;
 
+
+
 vai mostrar tudo que contenha maça no sabor e seja da garrafa pet.
--- consultas_mySQL-curso2/sql2/like.sql
+++ consultas_mySQL-curso2/sql2/like.sql
@@ -2,3 +2,5 @@ SELECT * FROM tabela_de_produtos WHERE SABOR LIKE '%Maça%';
 
 SELECT * FROM tabela_de_produtos WHERE SABOR LIKE '%Maça%'
 AND EMBALAGEM = 'pet';
+
+SELECT * FROM tabela_de_clientes WHERE nome LIKE '%Mattos%';
