914561ab3a703e5b96c8298d20c6a0cac0a424f0
danielemaraschin <danikmaraschin@gmail.com>
2020-12-25 18:08:48 +0100

    Learning about Distinct

---
+++ consultas_mySQL-curso2/sql2/distinct.sql
@@ -0,0 +1,6 @@
+select EMBALAGEM, TAMANHO FROM tabela_de_produtos;
+
+select distinct EMBALAGEM, TAMANHO FROM tabela_de_produtos;
+
+select distinct EMBALAGEM, TAMANHO FROM tabela_de_produtos
+where sabor = 'Laranja';
