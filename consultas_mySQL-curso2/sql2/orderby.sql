b2caa43de622084750b7c05fe67d6ba9f4038390
danielemaraschin <danikmaraschin@gmail.com>
2020-12-27 09:37:21 +0100

    Learning about order by

--- consultas_mySQL-curso2/sql2/orderby.sql
+++ consultas_mySQL-curso2/sql2/orderby.sql
@@ -1,3 +1,16 @@
 select * from tabela_de_produtos;
 
 select * from tabela_de_produtos order by preco_DE_lista;
+
+select * from tabela_de_produtos order by preco_DE_lista DESC;
+
+select * from tabela_de_produtos order by NOME_DO_PRODUTO;
+
+select * from tabela_de_produtos order by NOME_DO_PRODUTO DESC;
+
+select * from tabela_de_produtos order by EMBALAGEM, NOME_DO_PRODUTO;
+
+select * from tabela_de_produtos order by EMBALAGEM DESC, NOME_DO_PRODUTO ASC;
+
+select * from tabela_de_produtos where sabor like '%Morango%';
+select * from itens_notas_fiscais where codigo_do_produto = '1101035' order by QUANTIDADE DESC  ;
