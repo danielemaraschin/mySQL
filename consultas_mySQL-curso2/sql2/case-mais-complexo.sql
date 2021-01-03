93c62dc1c92d099ae188792d374396be309e100a
danielemaraschin <danikmaraschin@gmail.com>
2021-01-04 00:13:31 +0100

    Working with case, order by, group by, where, when

---
+++ consultas_mySQL-curso2/sql2/case-mais-complexo.sql
@@ -0,0 +1,13 @@
+select SABOR, embalagem,
+case
+	when preco_de_lista >= 12 then 'produto caro'
+	when preco_de_lista >= 7 and preco_de_lista < 12 then 'produto em conta'
+	else 'produto barato' 
+end as status_preco, AVG(PRECO_DE_LISTA) AS PREÇO_MÉDIO from tabela_de_produtos
+WHERE SABOR = 'MANGA'
+GROUP BY EMBALAGEM,
+case
+	when preco_de_lista >= 12 then 'produto caro'
+	when preco_de_lista >= 7 and preco_de_lista < 12 then 'produto em conta'
+	else 'produto barato' 
+end order by EMBALAGEM;
