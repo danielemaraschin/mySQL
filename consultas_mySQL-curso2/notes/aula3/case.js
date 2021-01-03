69757b1bf804baebaa805076e46ca6b06737ed9d
danielemaraschin <danikmaraschin@gmail.com>
2021-01-03 18:02:16 +0100

    CASE, WHEN, ELSE, THEN

--- consultas_mySQL-curso2/notes/aula3/case.js
+++ consultas_mySQL-curso2/notes/aula3/case.js
@@ -1,4 +1,14 @@
 case = if/else
 
 CASE = CASO tal coisa (condição)
-THEN = tipo de campo;
+THEN =pega um tipo de campo;
+
+ se não entrar em nenhuma das condições: ELSE
+
+
+ WHEN <condição 1> THEN < VALOR1>
+ WHEN <condição 2> THEN < VALOR2>
+ (...)
+ WHEN <condição n> THEN < VALORn>
+ ELSE valor ELSE
+ END
