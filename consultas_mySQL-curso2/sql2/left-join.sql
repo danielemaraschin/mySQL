644958153bcd60f0406bd7f05a48e41c38e0d682
danielemaraschin <danikmaraschin@gmail.com>
2021-01-05 20:10:02 +0100

    Left join

--- consultas_mySQL-curso2/sql2/left-join.sql
+++ consultas_mySQL-curso2/sql2/left-join.sql
@@ -7,4 +7,8 @@ select cpf, count(*) from notas_fiscais group by cpf;
 select distinct a.cpf, a.nome, b.cpf from tabela_de_clientes a
  inner join notas_fiscais b
  on a.cpf= b.cpf;
- 
+
+
+select distinct a.cpf, a.nome, b.cpf from tabela_de_clientes a
+left join notas_fiscais b
+ on a.cpf= b.cpf; 
