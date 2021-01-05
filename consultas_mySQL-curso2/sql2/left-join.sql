a40f3b8454740b2104acb5c77a4a1a8c20d2eaa7
danielemaraschin <danikmaraschin@gmail.com>
2021-01-05 20:08:31 +0100

    inner join

--- consultas_mySQL-curso2/sql2/left-join.sql
+++ consultas_mySQL-curso2/sql2/left-join.sql
@@ -1,3 +1,10 @@
 select count(*) from tabela_de_clientes;
 
 # eu usei o COUNT, não coloquei campo na frente, então não precisa usar GROUP BY.
+
+select cpf, count(*) from notas_fiscais group by cpf;
+
+select distinct a.cpf, a.nome, b.cpf from tabela_de_clientes a
+ inner join notas_fiscais b
+ on a.cpf= b.cpf;
+ 
