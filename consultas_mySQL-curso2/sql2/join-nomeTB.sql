b623eb39ac6cf5846eacc626a15d8b9405e13e25
danielemaraschin <danikmaraschin@gmail.com>
2021-01-06 15:41:57 +0100

    Learning about left and right join

--- consultas_mySQL-curso2/sql2/join-nomeTB.sql
+++ consultas_mySQL-curso2/sql2/join-nomeTB.sql
@@ -26,3 +26,25 @@ select tabela_de_vendedores.BAIRRO,
  tabela_de_clientes.NOME
  from tabela_de_vendedores inner join tabela_de_clientes
 on tabela_de_vendedores.BAIRRO = tabela_de_clientes.BAIRRO;
+
+
+select tabela_de_vendedores.BAIRRO,
+ tabela_de_vendedores.NOME, 
+ DE_FERIAS,
+ tabela_de_clientes.BAIRRO,
+ tabela_de_clientes.NOME
+ from tabela_de_vendedores LEFT join tabela_de_clientes
+on tabela_de_vendedores.BAIRRO = tabela_de_clientes.BAIRRO;
+
+#trouxe todos os vendedores e só os cliente correspondentes (ACIMA)
+
+# ABAIXO TRARÁ TODOS OS CLIENTES E SOMENTE OS VENDEDORES CORRESPONDENTES:
+
+
+select tabela_de_vendedores.BAIRRO,
+ tabela_de_vendedores.NOME, 
+ DE_FERIAS,
+ tabela_de_clientes.BAIRRO,
+ tabela_de_clientes.NOME
+ from tabela_de_vendedores right join tabela_de_clientes
+on tabela_de_vendedores.BAIRRO = tabela_de_clientes.BAIRRO;
