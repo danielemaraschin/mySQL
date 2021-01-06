38307e9235849aaa6b263e77ec41c3607c8bac41
danielemaraschin <danikmaraschin@gmail.com>
2021-01-06 13:39:41 +0100

    Working with join without labels

--- consultas_mySQL-curso2/sql2/join-nomeTB.sql
+++ consultas_mySQL-curso2/sql2/join-nomeTB.sql
@@ -5,4 +5,13 @@ SELECT * FROM tabela_de_clientes;
 SELECT count(*) FROM tabela_de_clientes;
 
 select * from tabela_de_vendedores inner join tabela_de_clientes
-	on tabela_de_vendedores.BAIRRO = tabela_de_clientes.BAIRRO
+	on tabela_de_vendedores.BAIRRO = tabela_de_clientes.BAIRRO;
+    
+    
+    
+select tabela_de_vendedores.BAIRRO,
+ tabela_de_vendedores.NOME ,
+ tabela_de_clientes.BAIRRO,
+ tabela_de_clientes.NOME
+ from tabela_de_vendedores inner join tabela_de_clientes
+on tabela_de_vendedores.BAIRRO = tabela_de_clientes.BAIRRO;
