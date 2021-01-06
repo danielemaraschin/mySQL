b2a2e07bf89a5497bc06f9a6c2ef410441fcd717
danielemaraschin <danikmaraschin@gmail.com>
2021-01-06 15:31:52 +0100

    inner join only fields

--- consultas_mySQL-curso2/sql2/join-nomeTB.sql
+++ consultas_mySQL-curso2/sql2/join-nomeTB.sql
@@ -10,7 +10,18 @@ select * from tabela_de_vendedores inner join tabela_de_clientes
     
     
 select tabela_de_vendedores.BAIRRO,
- tabela_de_vendedores.NOME ,
+ tabela_de_vendedores.NOME , 
+ tabela_de_clientes.BAIRRO,
+ tabela_de_clientes.NOME
+ from tabela_de_vendedores inner join tabela_de_clientes
+on tabela_de_vendedores.BAIRRO = tabela_de_clientes.BAIRRO;
+
+
+
+    
+select tabela_de_vendedores.BAIRRO,
+ tabela_de_vendedores.NOME, 
+ DE_FERIAS,
  tabela_de_clientes.BAIRRO,
  tabela_de_clientes.NOME
  from tabela_de_vendedores inner join tabela_de_clientes
