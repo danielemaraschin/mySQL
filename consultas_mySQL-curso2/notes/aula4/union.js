11c3ca741bf1a36f6fc61938d36c76e102611f98
danielemaraschin <danikmaraschin@gmail.com>
2021-01-07 19:41:52 +0100

    Learning about union and union ALL

--- consultas_mySQL-curso2/notes/aula4/union.js
+++ consultas_mySQL-curso2/notes/aula4/union.js
@@ -1,7 +1,10 @@
 UNION
 
 Faz a união de 2 ou mais tb;
-É necessário que tenham o mesmo numero de campos e sejam do mesmo tipo;
-
+É necessário que as tabelas tenham o mesmo numero de campos e sejam do mesmo tipo;
+Se existe duas informações iguais nas diferentes tabelas, essa informação só irá aparecer uma vez;
 
 UNION ALL
+
+Coloca todos os dados de todas as tabelas;
+Se algumas informações se repetirem, elas irão aparecer conforme o numero que aparecem nas tabelas
