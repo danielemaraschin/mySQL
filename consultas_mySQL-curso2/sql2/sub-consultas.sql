2012f5ff59744e2e80c06572000ab478c804aad8
danielemaraschin <danikmaraschin@gmail.com>
2021-01-08 12:22:28 +0100

    Learning about sub query

--- consultas_mySQL-curso2/sql2/sub-consultas.sql
+++ consultas_mySQL-curso2/sql2/sub-consultas.sql
@@ -8,3 +8,8 @@ in ('TIJUCA', 'JARDINS', 'SANTO AMARO');
 
 select * from tabela_DE_clientes where bairro
 in ('TIJUCA', 'JARDINS');
+
+#colocamos a nossa primeira consulta (linha1) dentro dos parenteses
+
+select * from tabela_DE_clientes where bairro
+in (select distinct bairro from tabela_de_vendedores);
