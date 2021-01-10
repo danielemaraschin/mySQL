838a9a6ee4226cf481cc2aba661f0783fe6a1bcf
danielemaraschin <danikmaraschin@gmail.com>
2021-01-10 14:13:58 +0100

    working with having count

--- consultas_mySQL-curso2/sql2/having-count.sql
+++ consultas_mySQL-curso2/sql2/having-count.sql
@@ -1,6 +1,8 @@
 #Quais foram os clientes que fizeram mais de 2000 compras em 2016?
 
-select VOLUME_DE_COMPRA  FROM TABELA_DE_CLIENTES;
+select cpf, count(*) as COMPRAS from notas_fiscais
+where YEAR(DATA_VENDA) = 2016 group by cpf 
+having count(*) > 2000;
 
   SELECT CPF, COUNT(*) FROM notas_fiscais
   WHERE YEAR(DATA_VENDA) = 2016
