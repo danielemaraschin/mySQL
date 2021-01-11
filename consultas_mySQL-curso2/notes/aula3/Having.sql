2cd69a9e83329f81ed136ebafd6f24496261215f
danielemaraschin <danikmaraschin@gmail.com>
2021-01-11 01:36:45 +0100

    Learning about having, sum, group by

--- consultas_mySQL-curso2/notes/aula3/Having.sql
+++ consultas_mySQL-curso2/notes/aula3/Having.sql
@@ -17,3 +17,17 @@
 select cpf, count(*) as numero_de_compras from notas_fiscais
 where year(data_venda) = 2016
 group by cpf having count(*) > 2000 ;
+
+
+--ex2:
+    --Mostrar qual estado tem a soma de valor de limite de credito superior a 900.000
+select estado, sum(limite_DE_credito) as soma_limite from tabela_de_clientes
+group by estado
+having sum(limite_DE_credito) > 900000;
+
+--ex.2.a
+--por exemplo, antes do ex2 pediriamos o limite de credito por estado .. dps pediriamos o ex2 q seria 
+--quais tem limite superior a 900000... 
+-- sem restringir um minimo do valor de limite de credito o cd seria assim:
+    select estado, sum(limite_DE_credito) as soma_limite from tabela_de_clientes
+group by estado;
