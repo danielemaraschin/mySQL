de76b4478dd17ef390754389cfd3fd4add6d55a4
danielemaraschin <danikmaraschin@gmail.com>
2021-01-11 01:58:03 +0100

    working with sub query

--- consultas_mySQL-curso2/sql2/having-subQuery.sql
+++ consultas_mySQL-curso2/sql2/having-subQuery.sql
@@ -1,4 +1,14 @@
+#usando having
+
   SELECT CPF, COUNT(*) FROM notas_fiscais
   WHERE YEAR(DATA_VENDA) = 2016
   GROUP BY CPF
   HAVING COUNT(*) > 2000;
+  
+  #usando sub-consultas
+  
+  select x.cpf, x.numero_vendas from 
+(select cpf ,  count(*) as numero_vendas from notas_fiscais
+where year(data_venda) = 2016
+group by cpf) x 
+where x.numero_vendas > 2000;
