e9ca451b70d093ad30a715667ae6d809ccdf36b6
danielemaraschin <danikmaraschin@gmail.com>
2021-01-13 20:29:47 +0100

    working with many filters

---
+++ introd-mySQL/Notes/aula5/FILTROS/FiltrosCompostos.sql
@@ -0,0 +1,7 @@
+--Filtros Compostos:
+
+ --       Entre cada um dos filtros , colocar:
+            and 
+--                ou 
+            or  
+--conforme o que quer buscar (bem simples, so juntar 2 filtros)
