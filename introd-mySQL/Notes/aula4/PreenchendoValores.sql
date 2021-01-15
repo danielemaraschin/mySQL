a1868876ea40102294b37e7c4a1f41f856a06e19
danielemaraschin <danikmaraschin@gmail.com>
2021-01-15 21:32:17 +0100

    Learning with: 'insert to'

---
+++ introd-mySQL/Notes/aula4/PreenchendoValores.sql
@@ -0,0 +1,14 @@
+--ADD LINHAS
+
+--Para add linhas (os valores) na tabela:
+
+    INSERT INTO nomeTabela (nomeDado1, nomeDado2, nomeDado3)
+    VALUES ('VALOR DADO1', 'VALORDADO2', VALOR, DADO3);
+  --          //COM ASPAS SIMPLES STRING, SEM ASPAS NUMEROS
+    --        //LEMBRANDO QUE NUMEROS COM DECIMAIS É PONTO E NÃO VÍRGULA.
+   
+      --  { Para cada Linha da tabela, toda essa declaração deve ser repetida}
+    
+    --Para inserir clicar no raio de executar.
+    --Para confirmar se os dados estao corretos abrir a tabelinha: 
+     SELECT * FROM nomeTabela;
