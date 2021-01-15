f570d182ce2a790991589ad658952ab742e6045e
danielemaraschin <danikmaraschin@gmail.com>
2021-01-15 21:30:29 +0100

    Learning about 'delete'

---
+++ introd-mySQL/Notes/aula4/delete.sql
@@ -0,0 +1,10 @@
+- --           delete
+
+--DELETE para apagar DADOS = informaçoes dentro da tabela.
+
+
+--Não confundir:
+
+-- DROP é para apagar componentes, objetos do MYSQL.
+  --Eu "dropo" o banco de dados, tabelas, índices, chaves primárias.
+ --Mas apagar é para dados. DELETE é para quando me refiro a informação dentro da tabela.
