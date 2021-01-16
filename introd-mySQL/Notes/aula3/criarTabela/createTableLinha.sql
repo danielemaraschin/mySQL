1ec87b1610ad3e1bf4ba397b2bc3cf9b5a09dce1
danielemaraschin <danikmaraschin@gmail.com>
2021-01-17 00:51:46 +0100

    Learning about filters

--- introd-mySQL/Notes/aula3/criarTabela/createTableLinha.sql
+++ introd-mySQL/Notes/aula3/criarTabela/createTableLinha.sql
@@ -1,5 +1,5 @@
---CREATE TABLE nomeTabela;
-
---(NOMEDOCAMPO1 tipoDoCampo1(Numero de digitos do campo),
-
---NOMEDOCAMPO2 tipoDoCampo2(Numero de digitos do campo),)
+-- CREATE TABLE nomeTabela;
+-- 
+-- (NOMEDOCAMPO1 tipoDoCampo1(Numero de digitos do campo),
+-- 
+-- NOMEDOCAMPO2 tipoDoCampo2(Numero de digitos do campo),)
--- introd-mySQL/Notes/aula5/FILTROS/FiltrosSimples.sql
+++ introd-mySQL/Notes/aula5/FILTROS/FiltrosSimples.sql
@@ -1,55 +1,56 @@
-FILTROS:
+                --FILTROS:
 
-1) Filtrando por apenas 1 característica
+--1) Filtrando por apenas 1 característica
 
-            { IGUALDADE }
+    --        { IGUALDADE }
 
     SELECT * FROM tbcliente WHERE CIDADE = 'Rio de Janeiro';
-        somente os clientes do rio
-ou
+  --      somente os clientes do rio
+--ou
     SELECT * FROM tbproduto WHERE SABOR = 'Cítricos';
-        Somente o produto sabor cítrico;
-ou
+  --      Somente o produto sabor cítrico;
+--ou
     SELECT * FROM tbcliente WHERE IDADE = 22;
 
 
 
-        {MAIOR, MENOR, MENOR OU IGUAL, DIFERENTE}
+  --      {MAIOR, MENOR, MENOR OU IGUAL, DIFERENTE}
 
-A) NÚMEROS
+--A) NÚMEROS
 
 
     SELECT * FROM tbcliente WHERE IDADE > 22;
-ou
+--ou
     SELECT * FROM tbcliente WHERE IDADE < 22;
-ou
+--ou
     SELECT * FROM tbcliente WHERE IDADE <= 22;
-ou 
+--ou 
     SELECT * FROM tbcliente WHERE IDADE <> 22; 
-<> sinal de diferente, ou seja, todos clientes que não tenham 22 anos
+--  <> sinal de diferente, ou seja, todos clientes que não tenham 22 anos
 
-
-        A.1)FLOAT: As condições igual, diferente, maior, menor, 
-                    maior ou igual e menor ou igual
-                    não se aplica muito bem a campos FLOAT
-            (pq é ponto flutuante, arredondado...)
+--
+--        A.1)FLOAT: As condições igual, diferente, maior, menor, 
+--                    maior ou igual e menor ou igual
+--                    não se aplica muito bem a campos FLOAT
+--            (pq é ponto flutuante, arredondado...)
 
     
 
 
-        A.1.a) BETWEEN 
-        é o comando usado para filtrar valores através do FLOAT.
-        Se sabe que existe por exemplo um valor 19.50 e quer encontrar ele usar o BETWEEN
+--    A.1.a) BETWEEN 
+--    é o comando usado para filtrar valores através do FLOAT.
+--    Se sabe que existe por exemplo um valor 19.50 e quer encontrar ele usar o BETWEEN
 
         SELECT * FROM tb_prod WHERE PRECO_LISTA between 19.49 AND 19.51;
 
 
- --   B) TEXTOS
-        => ordem alfabética
+ --B) TEXTOS
+ --    => ordem alfabética
 
-            
-            
- --           {BETWEEN AND}
+ --        
+ --        
+ --      
+ --        {BETWEEN AND}
 
         
     SELECT * FROM tbproduto WHERE PRECO_LISTA BETWEEN 16.007 AND 16.009;
