f5e57ec04b62277b2b263acb206821cbadf585ed
danielemaraschin <danikmaraschin@gmail.com>
2021-01-17 00:21:25 +0100

    create table and fields

--- introd-mySQL/Notes/aula3/criarTabela/createTableLinha.sql
+++ introd-mySQL/Notes/aula3/criarTabela/createTableLinha.sql
@@ -1,5 +1,5 @@
-CREATE TABLE nomeTabela;
+--CREATE TABLE nomeTabela;
 
-(NOMEDOCAMPO1 tipoDoCampo1(Numero de digitos do campo),
+--(NOMEDOCAMPO1 tipoDoCampo1(Numero de digitos do campo),
 
-NOMEDOCAMPO2 tipoDoCampo2(Numero de digitos do campo),)
+--NOMEDOCAMPO2 tipoDoCampo2(Numero de digitos do campo),)
