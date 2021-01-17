096056350f5f69790ca2d12f8d848073d3f66e1d
danielemaraschin <danikmaraschin@gmail.com>
2021-01-17 02:20:13 +0100

    working with LIKE

---
+++ consultas_mySQL-curso2/notes/aula2/like.sql
@@ -0,0 +1,11 @@
+--colocar %Dado%
+--
+SELECT * FROM tabela_de_produtos WHERE SABOR LIKE '%Maça%';
+--
+--Vai selecionar todos os sabore maçãs e com maça (que contenha maça, pode ter mais de um sabor)
+SELECT * FROM tabela_de_produtos WHERE SABOR LIKE '%Maça%' and 
+--embalagem = pet;
+--
+--
+--
+--vai mostrar tudo que contenha maça no sabor e seja da garrafa pet.
