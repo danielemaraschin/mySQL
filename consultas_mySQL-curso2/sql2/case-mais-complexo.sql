2c056a46aa72d005068be28be6eb0613334f248c
danielemaraschin <danikmaraschin@gmail.com>
2021-01-18 20:10:01 +0100

    working with case

--- consultas_mySQL-curso2/sql2/case-mais-complexo.sql
+++ consultas_mySQL-curso2/sql2/case-mais-complexo.sql
@@ -3,7 +3,7 @@ case
 	when preco_de_lista >= 12 then 'produto caro'
 	when preco_de_lista >= 7 and preco_de_lista < 12 then 'produto em conta'
 	else 'produto barato' 
-and as status_preco, AVG(PRECO_DE_LISTA) AS PREÇO_MÉDIO from tabela_de_produtos
+end as status_preco, AVG(PRECO_DE_LISTA) AS PREÇO_MÉDIO from tabela_de_produtos
 WHERE SABOR = 'MANGA'
 GROUP BY EMBALAGEM,
 case
