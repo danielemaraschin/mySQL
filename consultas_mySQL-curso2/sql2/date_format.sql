5302830a0d466d54234f239d24d3ff9c5d8fd129
danielemaraschin <danikmaraschin@gmail.com>
2021-01-26 18:33:02 +0100

    working with convert

--- consultas_mySQL-curso2/sql2/date_format.sql
+++ consultas_mySQL-curso2/sql2/date_format.sql
@@ -10,3 +10,12 @@ date_format (current_timestamp(), '%d/%m/%Y')) as hoje ;
 select concat ('o dia de hoje é: ', 
 date_format (current_timestamp(), '%d-%m-%y')) as hoje ;
 
+select concat ('o dia de hoje é: ', 
+date_format (current_timestamp(), '%W, %d-%m-%y')) as hoje ;
+
+select concat ('o dia de hoje é: ', 
+date_format (current_timestamp(), '%W ,%d-%m-%y - %U')) as hoje ;
+
+SELECT CONVERT (23.3, CHAR) AS RESULTADO;
+
+SELECT substring(CONVERT(23.3, CHAR),1, 1) AS RESULTADO;
